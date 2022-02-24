cac4d8719c8e159de88ce4206fed8e0dd329b253
Karine Moreira <karine.moreira@sealtelecom.com.br>
2022-02-24 18:01:30 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<script setup>
+  import "./main.css";
+  import { ref, computed } from "vue";
+
+  const header = ref("Shopping List App");
+  const editing = ref(false);
+  const items = ref([
+    { id: 1, label: "10 party hats", purchased: true, highPriority: false },
+    { id: 2, label: "2 board games", purchased: true, highPriority: false },
+    { id: 3, label: "20 cups", purchased: false, highPriority: true },
+  ]);
+  const reversedItems = computed(() => [...items.value].reverse());
+  const newItem = ref("");
+  const newItemHighPriority = ref("low");
+  const saveItem = () => {
+    items.value.push({
+      id: items.value.length + 1,
+      label: newItem.value,
+      highPriority: newItemHighPriority.value,
+    });
+    newItem.value = "";
+    newItemHighPriority.value = "";
+  };
+  const doEdit = (e) => {
+    editing.value = e;
+    newItem.value = "";
+    newItemHighPriority.value = "";
+  };
+  const togglePurchased = (item) => {
+    item.purchased = !item.purchased;
+  };
+</script>
+
+<template>
+  <div class="header">
+    <h1>{{ header }}</h1>
+    <button v-if="editing" class="btn" @click="doEdit(false)">Cancel</button>
+    <button v-else class="btn btn-primary" @click="doEdit(true)">
+      Add Item
+    </button>
+  </div>
+
+  <form class="add-item-form" v-if="editing" @submit.prevent="saveItem">
+    <input type="text" placeholder="Add an item" v-model.trim="newItem" />
+    <label>
+      <input type="checkbox" v-model="newItemHighPriority" />
+      High Priority
+    </label>
+    <button :disabled="newItem.length < 5" class="btn btn-primary">
+      Save Item
+    </button>
+  </form>
+  <ul>
+    <li
+      v-for="(item, index) in reversedItems"
+      @click="togglePurchased(item)"
+      class="static-class"
+      :key="item.id"
+      :class="{ strikeout: item.purchased, priority: item.highPriority }"
+    >
+      {{ item.label }}
+    </li>
+  </ul>
+</template>
